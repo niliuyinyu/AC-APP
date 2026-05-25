5a89abfb4f764c57df882ca82907e08feb3b1ede
user8253000532 <1136252682250812-user8253000532@noreply.coze.cn>
2026-05-26 03:22:50 +0800

    fix: 重新开启New Architecture以支持reanimated依赖

--- client/app.config.ts
+++ client/app.config.ts
@@ -15,7 +15,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     "icon": "./assets/images/icon.png",
     "scheme": "myapp",
     "userInterfaceStyle": "automatic",
-    "newArchEnabled": false,
+    "newArchEnabled": true,
     "ios": {
       "supportsTablet": true
     },
